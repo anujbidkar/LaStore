2471a695cd9e3fcfc04a2245b7367436241dbb38
FIRST_NAME LAST_NAME <singhetoos65@gmail.com>
2021-07-09 22:42:15 +0530

    build change

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App';
 import { Provider } from "react-redux";
 import { store } from './redux/Store'
+//  store connected with react for build development
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
